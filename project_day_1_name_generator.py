24d5438a3ee3d5049ca9c2421d0932e1beef3d83
Dilalovegood <91135063+Dilalovegood@users.noreply.github.com>
2024-08-14 10:56:47 +0700

    Add files via upload

---
+++ project_day_1_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Role Player Name Generator")
+favourite_character = input("Write your favourite character: ")
+favourite_things = input("Write your favourite things/words: ")
+print(f"Your Role Player Name is: {favourite_things}est {favourite_character}")
